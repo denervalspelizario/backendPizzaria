05ee4f493de974d2d0a965f28c1b4a588a25a56f
denervalspelizario <denervalspelizario@gmail.com>
2023-10-13 16:23:56 -0300

    update: Get/users rota cadastrod e usuario implementada

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,10 @@
 import { Router, Request, Response } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 
 const router = Router();
 
-router.get('/teste', (request: Request, response: Response) => {
-  return response.json({"nome": "Super Pizza"})
-})
+router.post('/users', new CreateUserController().handle)
 
 export {
   router
