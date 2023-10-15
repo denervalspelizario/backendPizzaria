0474ebf70c4e0dbda1a110d362000ecabb142c2e
denervalspelizario <denervalspelizario@gmail.com>
2023-10-14 22:11:03 -0300

    update criacao de rota criacaod e categoria controller CreateCategoryController e service CreateCategoryService

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 
 const router = Router();
@@ -16,8 +17,7 @@ router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 // Rotas CATEGORY
-
-
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export {
   router
