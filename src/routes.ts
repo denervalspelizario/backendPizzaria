e8c2ae5c93f69c80b121751aacdffd03f0247214
denervalspelizario <denervalspelizario@gmail.com>
2023-10-14 21:04:21 -0300

    update adicao de middleware isAuthenticated

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,23 @@
 import { Router, Request, Response } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 
 const router = Router();
 
+
+// Rotas de USER
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// Rotas CATEGORY
+
+
 
 export {
   router
